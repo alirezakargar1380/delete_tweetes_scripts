23665264d06d989c1a33c4536a274934b4a28eda
alireza kargar <alirezakargarsuport@gmail.com>
2024-05-18 15:19:15 +0330

    fixing bug finding & clicking on delete btn #1

--- delete_tweets.js
+++ delete_tweets.js
@@ -1,8 +1,6 @@
-// start - app settings
-//    check this setting before run this script
-
+/**--------------------- READ THIS BEFORE RUN ----------------**/
 /**
- *  Timeout
+ *  Timeout:
  *      this timeout is in seconds, if you set this to 5,
  *      every 5 second, this script will run 
  * 
@@ -11,12 +9,18 @@
  */
 var timeout = 5;
 
-// end - app settings
 
-// app variables
+/**--------------------- DON'T Change This SECTION - START ----------------**/
+
+// START - app variables
+//   NOTE:
 //      Don't change this
 var deletedTweets = 0;
 timeout = timeout * 1000
+
+// END - app settings
+
+/**--------------------- DON'T Change This SECTION - END ----------------**/
 
 var delTweets = function () {
     // get all your tweets number
@@ -24,18 +28,22 @@ var delTweets = function () {
     console.log('Remaining: ', tweetsRemaining + ', deleted:', deletedTweets);
 
     // delete tweets
-    document.querySelectorAll('[aria-label="More"]').forEach(function (v, i, a) {
-        v.click();
-        document.querySelectorAll('span').forEach(function (v2, i2, a2) {
-            if (v2.textContent === 'Delete') {
-                v2.click();
-                document.querySelectorAll('[data-testid="confirmationSheetConfirm"]').forEach(function (v3, i3, a3) {
-                    v3.click();
-                    deletedTweets++
-                });
-            } else {
-                document.body.click();
-            }
+    document.querySelectorAll('article[data-testid="tweet"]').forEach(function (tweetsArticle) {
+        tweetsArticle.querySelectorAll('[aria-label="More"]').forEach(async function (v, i, a) {
+            v.click();
+            
+            // IS TRUE
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            document.querySelectorAll('[role="menuitem"]').forEach(async function (v2, i2, a2) {
+                if (v2.textContent === 'Delete') {
+                    v2.click();
+                    await new Promise((resolve) => setTimeout(resolve, 500));
+                    document.querySelectorAll('[data-testid="confirmationSheetConfirm"]').forEach(function (v3, i3, a3) {
+                        v3.click();
+                        deletedTweets++
+                    });
+                }
+            });
         });
     });
 
